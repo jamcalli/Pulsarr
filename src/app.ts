973fb22baaaf9046a8580d1ce1336c3052badf9c
jamcalli <48490664+jamcalli@users.noreply.github.com>
2025-01-15 10:07:12 -0800

    Remove unused frontend files and configurations; add initial server setup and OpenAPI configuration

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,25 @@
 import Fastify from 'fastify';
 import AutoLoad from '@fastify/autoload';
 import { getDbInstance } from '@db/db';
-import { loggerConfig } from '@shared/logger/logger';
 import { getOpenapiConfig } from '@shared/config/openapi-config';
 import { getConfig } from '@shared/config/config-manager';
 import cors from '@fastify/cors';
 import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from 'fastify-type-provider-zod';
 import { gracefulShutdown } from '@shared/utils/app-specific';
 import { fastifySwagger } from '@fastify/swagger';
+import FastifyFormBody from '@fastify/formbody'
 
 export function build() {
   
   const server = Fastify({
-    logger: loggerConfig
+    logger: {
+      transport: {
+        target: '@fastify/one-line-logger'
+      }
+    }
   });
+
+  server.register(FastifyFormBody)
 
   gracefulShutdown(server);
 
@@ -30,7 +36,6 @@ export function build() {
   };
 
   server.register(cors, {
-    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
 
