8c84a28c874b40cc2293605ba9bb1a0f7fc9d024
Jayman McAllister <jayman.mcallister@gmail.com>
2025-04-01 00:01:50 -0700

    feat: add Plex notification configuration and removal functionality

--- src/routes/v1/plex/index.ts
+++ src/routes/v1/plex/index.ts
@@ -5,6 +5,8 @@ import { pingRoute } from '@routes/v1/plex/ping.js'
 import { generateRssFeedsRoute } from '@routes/v1/plex/generate-rss-feeds.js'
 import { rssWatchlistRoute } from '@routes/v1/plex/parse-rss.js'
 import { getGenresRoute } from '@routes/v1/plex/get-genres.js'
+import { configureNotificationsRoute } from '@routes/v1/plex/configure-notifications.js'
+import { removeNotificationsRoute } from '@routes/v1/plex/remove-notifications.js'
 
 const plexPlugin: FastifyPluginAsyncZod = async (fastify) => {
   await fastify.register(selfWatchlistTokenRoute)
@@ -13,6 +15,8 @@ const plexPlugin: FastifyPluginAsyncZod = async (fastify) => {
   await fastify.register(generateRssFeedsRoute)
   await fastify.register(rssWatchlistRoute)
   await fastify.register(getGenresRoute)
+  await fastify.register(configureNotificationsRoute)
+  await fastify.register(removeNotificationsRoute)
 }
 
 export default plexPlugin
