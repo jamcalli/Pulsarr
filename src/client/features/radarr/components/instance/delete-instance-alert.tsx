f62a464f4c5d0a9a7e093c3ab3fa304445a8811d
Jayman McAllister <jayman.mcallister@gmail.com>
2025-04-09 22:56:01 -0700

    refactor: remove unused DeleteGenreRouteAlert component from delete-instance-alert

--- src/client/features/radarr/components/instance/delete-instance-alert.tsx
+++ src/client/features/radarr/components/instance/delete-instance-alert.tsx
@@ -10,50 +10,6 @@ import {
 } from '@/components/ui/credenza'
 import { Button } from '@/components/ui/button'
 
-interface DeleteGenreRouteAlertProps {
-  open: boolean
-  onOpenChange: (open: boolean) => void
-  onConfirm: () => Promise<void>
-  routeName: string
-}
-
-export function DeleteGenreRouteAlert({
-  open,
-  onOpenChange,
-  onConfirm,
-  routeName,
-}: DeleteGenreRouteAlertProps) {
-  return (
-    <Credenza open={open} onOpenChange={onOpenChange}>
-      <CredenzaContent>
-        <CredenzaHeader>
-          <CredenzaTitle className="text-text">
-            Remove Genre Route?
-          </CredenzaTitle>
-          <CredenzaDescription>
-            {`Are you sure you want to remove the genre route "${routeName}"? This action cannot be undone.`}
-          </CredenzaDescription>
-        </CredenzaHeader>
-        <CredenzaBody>
-          <CredenzaFooter>
-            <CredenzaClose asChild>
-              <Button variant="neutral">Cancel</Button>
-            </CredenzaClose>
-            <Button
-              variant="clear"
-              onClick={() => {
-                onConfirm()
-              }}
-            >
-              Remove
-            </Button>
-          </CredenzaFooter>
-        </CredenzaBody>
-      </CredenzaContent>
-    </Credenza>
-  )
-}
-
 interface DeleteInstanceAlertProps {
   open: boolean
   onOpenChange: (open: boolean) => void
