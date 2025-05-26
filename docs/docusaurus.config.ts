87521976623bcfb18d72f1fae6845d98f4a80159
Jayman McAllister <jayman.mcallister@gmail.com>
2025-05-26 12:35:11 -0700

    feat: remove unused SVG file: undraw_docusaurus_tree.svg

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -140,7 +140,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/pulsarr-social-card.png',
     colorMode: {
       defaultMode: 'dark',
       disableSwitch: false,
