4b7c9993706c5967673eabc284f96f63b810b713
Jayman McAllister <jayman.mcallister@gmail.com>
2025-11-10 11:36:07 -0800

    feat: refactor pure utility functions from plex-server.service into modules

---
+++ src/services/plex-server/metadata/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Metadata Module
+ *
+ * Provides operations for searching and retrieving content metadata from Plex.
+ */
+
+export { getShowMetadata, searchByGuid } from './metadata-operations.js'
